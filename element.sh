ac76cc8131b494cafd1fc7a9649a5bbc281b4781
rodneyb24 <115502580+rodneyb24@users.noreply.github.com>
2023-02-06 21:34:50 -0800

    element.sh

---
+++ element.sh
@@ -0,0 +1,36 @@
+#! /bin/bash
+
+PSQL="psql --username=freecodecamp --dbname=periodic_table --no-align --tuples-only -c"
+
+
+# MAIN_MENU() {
+  if [[ $1 ]]
+  then
+  
+    # echo -e "\n$1"
+    # if not a number
+      if [[ ! $1 =~ ^[0-9]+$ ]]
+      then
+      ELEMENT=$($PSQL "SELECT atomic_number, atomic_mass, melting_point_celsius, boiling_point_celsius, symbol, name, type FROM properties JOIN elements USING(atomic_number) JOIN types USING(type_id) WHERE elements.name LIKE '$1%' ORDER BY atomic_number LIMIT 1")
+      
+    
+      
+      else
+      ELEMENT=$($PSQL "SELECT atomic_number, atomic_mass, melting_point_celsius, boiling_point_celsius, symbol, name, type FROM properties JOIN elements USING(atomic_number) JOIN types USING(type_id) WHERE elements.atomic_number=$1")
+      fi
+    
+         if [[ -z $ELEMENT ]]
+  then
+  echo "I could not find that element in the database."
+  else
+  echo "$ELEMENT" | while IFS=\| read ATOMIC_NUMBER ATOMIC_MASS MELTING_POINT_CELSIUS BOI SY NA TYPE
+      do
+        echo  "The element with atomic number $ATOMIC_NUMBER is $NA ($SY). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NA has a melting point of $MELTING_POINT_CELSIUS celsius and a boiling point of $BOI celsius."
+      done
+  fi
+
+    else
+    echo  "Please provide an element as an argument."
+   
+  fi
+
